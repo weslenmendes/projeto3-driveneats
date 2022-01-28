b6269c6b004ad9ceb52ed2657322fbd1a2c411cc
Weslen Mendes <weslenmends@gmail.com>
2022-01-28 20:50:43 -0300

    Modified some variables in script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-let dishes, drinks, desserts, totalSum;
+let dishes = null,
+  drinks = null,
+  desserts = null,
+  totalSum = null;
 
 function fillOrderVariables(attribute, e) {
-  if (attribute === 'dishes') {
+  if (attribute === "dishes") {
     dishes = e;
-  } else if (attribute === 'drinks') {
+  } else if (attribute === "drinks") {
     drinks = e;
   } else {
     desserts = e;
@@ -22,11 +25,11 @@ function checkIfOrdersAreMarked() {
 function takeOrders(e) {
   const attribute = e.parentElement.getAttribute("data-identifier");
 
-  const section = document.querySelector(`[data-identifier="${attribute}"]`);  
+  const section = document.querySelector(`[data-identifier="${attribute}"]`);
   const foods = section.querySelectorAll('[data-identifier="food-option"]');
 
-  foods.forEach(food => {
-      food.classList.remove("checked");
+  foods.forEach((food) => {
+    food.classList.remove("checked");
   });
 
   e.classList.add("checked");
@@ -46,25 +49,34 @@ function makeOrder() {
     modalItems.forEach((modalItem, index) => {
       let title = modalItem.querySelector(".modal__item-title");
       let price = modalItem.querySelector(".modal__item-price");
-  
+
       if (index === 0) {
         title.innerText = dishes.querySelector(".title").innerText;
-        price.innerText = dishes.querySelector(".value").innerText.split(" ")[1];
+        price.innerText = dishes
+          .querySelector(".value")
+          .innerText.split(" ")[1];
       } else if (index === 1) {
         title.innerText = drinks.querySelector(".title").innerText;
-        price.innerText = drinks.querySelector(".value").innerText.split(" ")[1];
+        price.innerText = drinks
+          .querySelector(".value")
+          .innerText.split(" ")[1];
       } else {
         title.innerText = desserts.querySelector(".title").innerText;
-        price.innerText = desserts.querySelector(".value").innerText.split(" ")[1];
+        price.innerText = desserts
+          .querySelector(".value")
+          .innerText.split(" ")[1];
       }
 
       total += parseFloat(price.innerText.replace(",", "."));
     });
 
     totalSum = total.toFixed(2);
-    total = total.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    total = total.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
     sum.innerText = total;
-  
+
     modal.classList.add("show");
   }
 }
@@ -75,8 +87,8 @@ function sendOrderToWhatsapp() {
   let msg = `Olá, gostaria de fazer o pedido:\n- Prato: ${dishes.children[1].innerText}\n- Bebida: ${drinks.children[1].innerText}\n- Sobremesa: ${desserts.children[1].innerText}\nTotal: R$ ${totalSum}\n\nNome: ${nome}\nEndereço: ${endereco}\n
   `;
 
-  msg = window.encodeURIComponent(msg); 
-  
+  msg = window.encodeURIComponent(msg);
+
   window.open(`https://wa.me/5584981018236?text=${msg}`, "_blank");
 }
 
